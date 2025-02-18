9a540272aa38b86d1d21d825e418187071360ca2
Elliott Grieco <elliott@Elliotts-MacBook-Pro.local>
2025-02-18 17:10:03 -0500

    Use eleventy to build a basic blog page

---
+++ _data/metadata.js
@@ -0,0 +1,11 @@
+export default {
+	title: "Elloott Grieco",
+	url: "https://websiteaboutmy.website",
+	language: "en",
+	description: "A porflolio and blog about Elliott Grieco",
+	author: {
+		name: "Elliott Grieco",
+		email: "elliott.grieco@gmail.com",
+		url: "https://websiteaboutmy.website"
+	}
+}
