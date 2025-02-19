1eb983d78c2ac85d93827cf1cdb6cf64e5772007
Elliott Grieco <elliott.grieco@gmail.com>
2025-02-18 20:13:29 -0500

    Eleventyify (#2)
    * Use eleventy to build a basic blog page
    
    * Some more eleventy iteration
    
    * Fix root page, add a quick blog link, first dummy post
    
    * Remove legacy sass, fix CSS
    
    ---------
    
    Co-authored-by: Elliott Grieco <elliott@Elliotts-MacBook-Pro.local>

---
+++ _data/metadata.js
@@ -0,0 +1,11 @@
+export default {
+	title: "Elliott Grieco",
+	url: "https://websiteaboutmy.website",
+	language: "en",
+	description: "A porflolio and blog about Elliott Grieco",
+	author: {
+		name: "Elliott Grieco",
+		email: "elliott.grieco@gmail.com",
+		url: "https://websiteaboutmy.website"
+	}
+}
