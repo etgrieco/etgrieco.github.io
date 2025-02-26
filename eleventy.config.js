984cd8fe303fb80d4d7c75e245e3ed37a3244775
Elliott Grieco <elliott.grieco@gmail.com>
2025-02-26 14:39:55 -0500

    Fix base metadta, used for feeds

--- eleventy.config.js
+++ eleventy.config.js
@@ -69,7 +69,7 @@ export default async function (eleventyConfig) {
 			language: "en",
 			title: "Blog Title",
 			subtitle: "This is a longer description about your blog.",
-			base: "https://example.com/",
+			base: "https://websiteaboutmy.website/",
 			author: {
 				name: "Your Name",
 			},
